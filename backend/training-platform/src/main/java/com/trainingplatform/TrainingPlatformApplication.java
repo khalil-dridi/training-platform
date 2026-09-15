7da1932d77b646c3347f11b209ed592174f04923
Khalil Dridi <khalil.dridi@esprit.tn>
2026-09-15 15:40:49 +0100

    testing github hook

--- backend/training-platform/src/main/java/com/trainingplatform/TrainingPlatformApplication.java
+++ backend/training-platform/src/main/java/com/trainingplatform/TrainingPlatformApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrainingPlatformApplication {
+    //test
 
     public static void main(String[] args) {
         SpringApplication.run(TrainingPlatformApplication.class, args);
